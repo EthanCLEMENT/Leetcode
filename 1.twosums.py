9fac5f5772208876b45865002aeea76edede9b0c
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-07-08 11:30:33 +0200

    Add files via upload

---
+++ 1.twosums.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+       seen = {}
+       for i, value in enumerate(nums):
+           remaining = target - nums[i]
+           
+           if remaining in seen:
+               return [i, seen[remaining]]
+            
+           seen[value] = i 
