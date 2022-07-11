9eff0c11adc6366e5602bdf347766f3d4f3008d2
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-07-11 16:41:47 +0200

    Create 771.py

---
+++ 771.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        for i in jewels:
+            for j in stones:
+                if i == j:
+                    count += 1
+        return count
+
+        
