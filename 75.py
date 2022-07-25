cf06c51f359aec2af56659363f41ef32080c236e
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-07-25 19:37:14 +0200

    Create 75.py

---
+++ 75.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            for j in range(0, len(nums) - i - 1):
+                if nums[j] > nums[j + 1]:
+                    temp = nums[j]
+                    nums[j] = nums[j+1]
+                    nums[j+1] = temp
+        return nums
