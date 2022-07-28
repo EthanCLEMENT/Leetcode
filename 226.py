74de5c9c74af2769572923fca54fbc7c71cc04e5
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-07-28 10:07:36 +0200

    Create 226.py

---
+++ 226.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root:
+            left = root.left
+            right = root.right
+            root.left = right
+            root.right = left
+            self.invertTree(root.left)
+            self.invertTree(root.right)
+        return root
+        
