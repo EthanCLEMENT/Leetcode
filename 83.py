6a17a5ad469d460ef9e94540f712394f36fa919f
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-07-29 17:11:41 +0200

    Update 83.py

--- 83.py
+++ 83.py
@@ -4,10 +4,15 @@ class ListNode:
         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 1
         begin = head
         while begin and begin.next:
+            # if 1 == 1 then 1 = 2
+            # 1 2 2
+            # 1 2
             if begin.val == begin.next.val:
                 begin.next = begin.next.next
             else:
                 begin = begin.next
         return head
+
