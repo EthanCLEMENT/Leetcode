fa96c54d7cafdae41cec913b395d2cacc0ef964a
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-22 00:51:08 +0200

    Update 94.py

--- 94.py
+++ 94.py
@@ -1,14 +1,17 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-        if root:
-            ans += self.inorderTraversal(root.left)
-            ans.append(root.val)
-            ans += self.inorderTraversal(root.right)
-        return ans 
-        
+        return self.dfs(root,[])
+    
+    def dfs(self, root, arr):
+        if root is None:
+            return
+        self.dfs(root.left,arr)
+        arr.append(root.val)
+        self.dfs(root.right,arr)
+        return arr
