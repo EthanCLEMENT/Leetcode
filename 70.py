5f6dc1ae150a95a65781044e77f915f9a8e0b777
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-22 10:01:40 +0200

    Update 70.py

--- 70.py
+++ 70.py
@@ -1,11 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        if n == 1:
-            return 1
-        dp[1] = 1
-        dp[2] = 2
+        dp = []
+        dp.append(1)
+        dp.append(2)
 
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
-        return dp[n]
+        for i in range(2,n):
+            dp.append(dp[i-1]+dp[i-2])
+        return dp[n-1]
