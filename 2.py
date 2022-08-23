d0ef0cae70cd6deba0f52dd0b7a66b37fed23e81
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-23 10:52:07 +0200

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        n=""
+        m=""
+        while l1 is not None:
+            n+=str(l1.val)
+            l1=l1.next
+        while l2 is not None:
+            m+=str(l2.val)
+            l2=l2.next
+        ans=str(int(m[::-1]) + int(n[::-1]))
+        LL=ListNode()
+        for i in ans:
+            newNode=ListNode(i)
+            newNode.next=LL.next
+            LL.next=newNode
+        return LL.next
+        
+    
