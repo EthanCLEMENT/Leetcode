9d9046d5b8b0ad8bac3e5bb4333b3b25fa59a58a
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-23 15:56:47 +0200

    Update 101.py

--- 101.py
+++ 101.py
@@ -1,18 +1,16 @@
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if root is None:
+    def dfs(self,root1,root2):
+        if root1==None and root2==None:
             return True
-        def solve(left_root, right_root):
-            if left_root == None or right_root == None:
-                return left_root == right_root
-            if left_root.val != right_root.val:
-                return False
-            return solve(left_root.left, right_root.right) and solve(left_root.right, right_root.left)
-        return solve(root.right, root.left)
-
-        
+        if (root1==None and root2!=None) or (root2==None and root1!=None) or (root1.val!=root2.val):
+            return False
+        return self.dfs(root1.left,root2.right) and self.dfs(root1.right,root2.left)
+    
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.dfs(root.left,root.right)
