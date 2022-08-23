ea9c7e7a09cc045cff63760e5a8b8b7fd169bbd7
Ethan Clement <80329688+EthanCLEMENT@users.noreply.github.com>
2022-08-23 11:25:05 +0200

    Create 23.py

---
+++ 23.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if lists is None:
+            return None
+        ans=[]
+        for i in lists:
+            while i is not None:
+                ans.append(i.val)
+                i=i.next
+        ans.sort()
+        LL=ListNode()
+        for i in ans[::-1]:
+            newNode=ListNode(i)
+            newNode.next=LL.next
+            LL.next=newNode
+        return LL.next
